b1200b3649543d5677ae92ff73f3a0346c33658b
Edward Ro <ero@stuy.edu>
2017-01-12 10:58:18 -0500

    added Character.java

---
+++ Character.java
@@ -0,0 +1,33 @@
+public class Character {
+    
+    //===Instance Variables===
+    protected int HP;
+    protected String name;
+    protected int condition;
+
+
+    //===Constructors===
+    public Character() {
+	HP = 100;
+	condition = 0;
+    }
+    public Character(String x) {
+	this();
+	name = x;
+    }
+
+
+    //===Methods===
+    public boolean isAlive() {
+	return HP > 0;
+    }
+    public String getName() {
+	return name;
+    }
+    public int getHP() {
+	return HP;
+    }
+    public int getCondition() {
+	return condition;
+    }
+}
