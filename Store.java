c1df0fe04405156472b9ef0087fad6ddb94d0068
Ero2000 <ero@stuy.edu>
2017-01-20 15:37:13 -0500

    Update Store.java

--- Store.java
+++ Store.java
@@ -15,10 +15,34 @@ public class Store {
     public static void enterStore() {
 	System.out.println ("Welcome to our general store. What would you like to buy?");
 	System.out.println ("We have:\n\n" + "1.\tFood\n" + "2.\tAmmo\n" + "3.\tTongues\n" + "4.\tWheels\n" + "5.\tAxles\n" + "6.\tLeave the store.\n\n");
-	    
+	
+	int answer = Keyboard.readInt();
+	if (answer == 1) buyFood();	
+	if (answer == 2) buyAmmo();
+	if (answer == 3) buyTongues();
+	if (answer == 4) buyWheels();
+	if (answer == 5) buyAxles();
     }
 
     public void buyFood() {
-	    
+	    	System.out.println ("Food costs " + foodP + " per parcel. Each parcel contains 10 pounds of food.\n" + "How many parcels would you like to buy? ");
+		answer = Keyboard.readInt();
+		while (Family.john.getMoney() < (foodP * answer)) {
+			System.println ("You do not have enough money. Please input a different amount.");
+			answer = Keyboard.readInt();
+		}
+		Family.john.addFood(answer * 10);
+	    	Family.john.subMoney(answer * foodP);
+    }
+	
+    public void buyAmmo() {
+	    	System.out.println ("Ammo goes for " + ammoP + " per box of bullets. Each box contains 20 bullets.\n" + "How many parcels would you like to buy? ");
+		answer = Keyboard.readInt();
+		while (Family.john.getMoney() < (ammoP * answer)) {
+			System.println ("You do not have enough money. Please input a different amount.");
+			answer = Keyboard.readInt();
+		}
+		Family.john.addAmmo(answer * 20);
+	    	Family.john.subMoney(answer * ammoP);
     }
 }
