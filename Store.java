3a32198e590098f0602e0e4a1e9538c61e87c1c8
Edward Ro <ero@stuy.edu>
2017-01-20 11:08:56 -0500

    Started working on Store.java and Town.java

---
+++ Store.java
@@ -0,0 +1,20 @@
+public class Store {
+    private int foodP;
+    private int ammoP;
+    private int tonguesP;
+    private int wheelsP;
+    private int axlesP;
+
+    public Store(int f, int a, int t, int w, int a) {
+	foodP = f;
+	ammoP = a;
+	tonguesP = t;
+	wheelsP = w;
+	axlesP = a;
+    }
+    public static void enterStore() {
+	System.out.println ("Welcome to " + 
+    }
+
+    public void buyFood() 
+}
---
+++ Town.java
@@ -0,0 +1,8 @@
+public class Town{
+
+    public Town(int f, int a, int t, int w, int ax) {
+	Store Goods = new Store(f, a, t, w, ax);
+    }
+
+    
+}
